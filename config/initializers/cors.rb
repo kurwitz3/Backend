f3c138fe0a8be5c5d8adb8d15336be4fcddf70e5
kurwitz3 <kurwitz3@aol.com>
2021-04-10 22:00:19 -0400

    heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
    allow do
-     origins '*'
+     origins 'https://wonderful-lamarr-3ee816.netlify.app'
 
      resource '*',
        headers: :any,
